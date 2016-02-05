44756ceaccead05f0f37acc912e0c750bd37c35c
revangelista <royevangelista@gmail.com>
2016-02-05 15:19:18 +0800

    Fix wrong import.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import dateutil.parser
-from settings import CREDENTIALS, PROJECTS, UPDATED_SINCE
+from settings import CREDENTIALS, PROJECTS, UPDATED_SINCE_HOURS
 from jiracopy import JiraLogCopier
 
-jira_log_copier = JiraLogCopier(CREDENTIALS, PROJECTS, UPDATED_SINCE)
+jira_log_copier = JiraLogCopier(CREDENTIALS, PROJECTS, UPDATED_SINCE_HOURS)
 jira_log_copier.copy_worklogs()
 print "Done."
